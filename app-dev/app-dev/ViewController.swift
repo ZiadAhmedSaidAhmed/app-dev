adc308c39280115942b13ca6b8fa81d8554d9452
ziad <ziadelnady555@gmail.com>
2020-06-07 10:49:57 -0700

    itialize app-dev app

---
+++ app-dev/app-dev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-dev
+//
+//  Created by Ziad Ahmed Said Ahmed on 6/7/20.
+//  Copyright © 2020 Ziad Ahmed Said Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
